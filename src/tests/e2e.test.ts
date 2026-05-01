3218c4020ebe6ad876884f6ddc414e66f520e676
PabloFalc <pablopatricio640@gmail.com>
2026-05-01 13:47:54 -0300

    feat

--- src/tests/e2e.test.ts
+++ src/tests/e2e.test.ts
@@ -1,8 +1,8 @@
-import type { WebDriver } from 'selenium-webdriver';
-import { createDriver } from '@/config/selenium.driver';
-import { addProductsToCartAndOpenCart } from './flows/cart.flow';
-import { completeCheckout } from './flows/checkout.flow';
-import { login } from './flows/login.flow';
+import type { WebDriver } from "selenium-webdriver";
+import { createDriver } from "@/config/selenium.driver";
+import { addProductsToCartAndOpenCart } from "./flows/cart.flow";
+import { completeCheckout } from "./flows/checkout.flow";
+import { login } from "./flows/login.flow";
 
 let driver: WebDriver;
 
@@ -12,25 +12,25 @@ beforeEach(async () => {
 
 const timeout = 20 * 1000;
 
-jest.setTimeout(60 * 1000);
+jest.setTimeout(120 * 1000);
 
-describe('Teste completo e2e de Swag Labs', () => {
-  it('deve fazer login', async () => {
-    await login(driver, timeout);
-  });
+describe("Teste completo e2e de Swag Labs", () => {
+  // it('deve fazer login', async () => {
+  //   await login(driver, timeout);
+  // });
 
-  it('deve adicionar produtos ao carrinho', async () => {
-    await login(driver, timeout);
-    await addProductsToCartAndOpenCart(driver, timeout);
-  });
+  // it('deve adicionar produtos ao carrinho', async () => {
+  //   await login(driver, timeout);
+  //   await addProductsToCartAndOpenCart(driver, timeout);
+  // });
 
-  it('deve concluir checkout', async () => {
-    await login(driver, timeout);
-    await addProductsToCartAndOpenCart(driver, timeout);
-    await completeCheckout(driver, timeout);
-  });
+  // it('deve concluir checkout', async () => {
+  //   await login(driver, timeout);
+  //   await addProductsToCartAndOpenCart(driver, timeout);
+  //   await completeCheckout(driver, timeout);
+  // });
 
-  it('deve fazer o fluxo completo', async () => {
+  it("deve fazer o fluxo completo", async () => {
     await login(driver, timeout);
     await addProductsToCartAndOpenCart(driver, timeout);
     await completeCheckout(driver, timeout);
