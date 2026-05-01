1e2eef2b046a31d762056d5d01be7edbecd379cb
PabloFalc <pablopatricio640@gmail.com>
2026-05-01 02:02:42 -0300

    teste

--- src/config/selenium.driver.ts
+++ src/config/selenium.driver.ts
@@ -1,12 +1,14 @@
 import { Builder } from "selenium-webdriver";
-import chrome from "selenium-webdriver/chrome.js";
+import chrome from "selenium-webdriver/chrome";
 
 export async function createDriver() {
   const options = new chrome.Options();
 
-  options.addArguments("--headless=new");
+  options.addArguments("--headless");
   options.addArguments("--no-sandbox");
   options.addArguments("--disable-dev-shm-usage");
+  options.addArguments("--disable-gpu");
+  options.addArguments("--window-size=1920,1080");
 
   const driver = await new Builder()
     .forBrowser("chrome")
