d5984e31d210060b7a20df2f4f16b4938453c338
PabloFalc <pablopatricio640@gmail.com>
2026-05-01 04:29:49 -0300

    feat

--- src/config/selenium.driver.ts
+++ src/config/selenium.driver.ts
@@ -4,7 +4,7 @@ import chrome from "selenium-webdriver/chrome";
 export async function createDriver() {
   const options = new chrome.Options();
 
-  options.addArguments("--headless");
+  options.addArguments("--headless=new");
   options.addArguments("--no-sandbox");
   options.addArguments("--disable-dev-shm-usage");
   options.addArguments("--disable-gpu");
